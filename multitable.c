9f88ac214be88ce3664d9b2bd22dc3f25b48e689
keerthivivekanandhan <30337466+keerthivivekanandhan@users.noreply.github.com>
2017-08-26 16:42:56 +0530

    Create multitable.c

---
+++ multitable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,m;
+    scanf("%d ",&n);
+    for(i=1;i<=10;i++)
+    {
+        m=n*i;
+        printf(" %d * %d =%d\n",n,i,m);
+    }
+    return 0;
+}
